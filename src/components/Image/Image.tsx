71d1d75e7eab0c4c1282ec251ee49fc2f91e2c76
MichelleW31 <mattie.williams31@gmail.com>
2022-12-14 16:20:27 -0700

    add login layout

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,7 +1,7 @@
 import styles from './Image.module.scss';
 
 const Image = () => (
-  <section className={styles.ImageWrapper}>
+  <section className={styles.ImageContainer}>
     <h2 className={styles.HeaderCopy}>Welcome to BGM Bank!</h2>
     <p className={styles.BodyCopy}>Let's Get Started</p>
   </section>
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,24 @@
 import styles from './Login.module.scss';
 import Image from 'components/Image/Image';
+import {useState} from 'react';
 
 const Login = () => {
+  const [userName, setUserName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [errorStatus, setErrorStatus] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
   return (
     <section className={styles.LoginContainer}>
       <Image />
+      <section className={styles.LoginForm}>
+        <h2 className={styles.FormHeaderCopy}>Login</h2>
+        <input type='text' placeholder='Username' value={userName} onChange={(e) => setUserName(e.target.value)} />
+        <input type='text' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        {errorStatus && <p className={styles.Error}>{error}</p>}
+        <button className={styles.LoginButton}>Login</button>
+        <p className={styles.ForgotPasswordCopy}>Forgot Password</p>
+      </section>
     </section>
   );
 };
